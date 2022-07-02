b29959c93adb1a41ba3433f1fba8b4442a74ce6c
A5TR0spud <supercellstage@gmail.com>
2022-07-02 10:30:09 -0400

    MORE

--- src/main/java/net/astrospud/ccastroadds/item/ModItems.java
+++ src/main/java/net/astrospud/ccastroadds/item/ModItems.java
@@ -12,7 +12,7 @@ public class ModItems {
 
     // new Item(new Item.Settings().maxCount(1).group(ChestCavity.ORGAN_ITEM_GROUP));
 
-    //echo
+    //echo set
     public static final Item ABYSSAL_HEART = registerItem("abyssal_heart",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item ABYSSAL_LUNG = registerItem("abyssal_lung",
@@ -20,7 +20,7 @@ public class ModItems {
     public static final Item ABYSSAL_MUSCLE = registerItem("abyssal_muscle",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
 
-    //copper
+    //copper set
     public static final Item ACTUATOR = registerItem("actuator",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item COPPER_WIRING = registerItem("copper_wiring",
@@ -34,7 +34,7 @@ public class ModItems {
     public static final Item BLOOD_PUMP = registerItem("blood_pump",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
 
-    //gold
+    //gold set
     public static final Item JUMP_SHAFT = registerItem("jump_shaft",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item GOLDEN_WIRING = registerItem("golden_wiring",
@@ -44,13 +44,28 @@ public class ModItems {
     public static final Item GEM_HOLSTER = registerItem("gem_holster",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
 
-    //iron
+    //iron set
     public static final Item STEEL_ACTUATOR = registerItem("steel_actuator",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item STEEL_FRAME = registerItem("steel_frame",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+    public static final Item STEEL_DETOXIFIER = registerItem("steel_detoxifier",
+            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+    public static final Item STEEL_CORE = registerItem("steel_core",
+            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+
+    //utility
+    public static final Item CAPTURED_SOUL = registerItem("captured_soul",
+            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+    public static final Item CREATIVE_SOUL = registerItem("creative_soul",
+            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+
     public static final Item IRON_LUNG = registerItem("iron_lung",
             new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+    public static final Item HEART_OF_GOLD = registerItem("heart_of_gold",
+            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+    public static final Item HASTY_MUSCLE = registerItem("hasty_muscle",
+            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(CCAstroAdds.MOD_ID, name), item);
