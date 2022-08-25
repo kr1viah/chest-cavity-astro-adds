a9f8c210b03a012991ad73ce89b437d9eacff60e
A5TR0spud <supercellstage@gmail.com>
2022-08-25 18:51:50 -0400

    a

--- src/main/java/net/astrospud/ccastroadds/registration/CCAAOrganScores.java
+++ src/main/java/net/astrospud/ccastroadds/registration/CCAAOrganScores.java
@@ -14,4 +14,5 @@ public class CCAAOrganScores extends CCOrganScores {
     public static final Identifier FLIGHT = new Identifier(CCAstroAdds.MOD_ID, "flight");
     public static final Identifier IRON_GUT = new Identifier(CCAstroAdds.MOD_ID, "iron_gut");
     public static final Identifier EAT_THE_RICH = new Identifier(CCAstroAdds.MOD_ID, "eat_the_rich");
+    public static final Identifier SOLAR_PLEXUS = new Identifier(CCAstroAdds.MOD_ID, "solar_plexus");
 }
