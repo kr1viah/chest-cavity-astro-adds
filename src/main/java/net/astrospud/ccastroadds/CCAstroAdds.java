44504e2bc685c9b02e7a67ef6c6320a5b8fdf258
A5TR0spud <supercellstage@gmail.com>
2022-09-11 20:53:38 -0400

    New item group

--- src/main/java/net/astrospud/ccastroadds/CCAstroAdds.java
+++ src/main/java/net/astrospud/ccastroadds/CCAstroAdds.java
@@ -9,6 +9,11 @@ import net.astrospud.ccastroadds.listeners.CCAAOrganTickListeners;
 import net.astrospud.ccastroadds.registration.CCAAStatusEffects;
 import net.astrospud.ccastroadds.registration.CCAAItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import net.tigereye.chestcavity.registration.CCItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +21,10 @@ public class CCAstroAdds implements ModInitializer {
 	public static CCAAConfig config;
 	public static final String MOD_ID = "ccastroadds";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final ItemGroup ORGAN_ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier(MOD_ID, "organs"),
+			() -> new ItemStack(CCAAItems.STEEL_DISTRIBUTOR));
 
 	@Override
 	public void onInitialize() {
--- src/main/java/net/astrospud/ccastroadds/registration/CCAAItems.java
+++ src/main/java/net/astrospud/ccastroadds/registration/CCAAItems.java
@@ -11,118 +11,118 @@ import net.tigereye.chestcavity.ChestCavity;
 
 public class CCAAItems {
 
-    // new Item(new Item.Settings().maxCount(1).group(ChestCavity.ORGAN_ITEM_GROUP));
+    // new Item(new Item.Settings().maxCount(1).group(CCAstroAdds.ORGAN_ITEM_GROUP));
 
     //echo set
     public static final Item ABYSSAL_HEART = registerItem("abyssal_heart",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item ABYSSAL_LUNG = registerItem("abyssal_lung",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item ABYSSAL_MUSCLE = registerItem("abyssal_muscle",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(16)));
 
     //copper set
     public static final Item ACTUATOR = registerItem("actuator",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item COPPER_WIRING = registerItem("copper_wiring",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(4)));
     public static final Item COPPER_FRAME = registerItem("copper_frame",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(4)));
     public static final Item PHANTOM_PUMP = registerItem("phantom_pump",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item FILTER_PUMP = registerItem("filter_pump",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item BLOOD_PUMP = registerItem("blood_pump",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     //gold set
     public static final Item JUMP_SHAFT = registerItem("jump_shaft",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item GOLDEN_WIRING = registerItem("golden_wiring",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(4)));
     public static final Item CLOCKWORK_CORE = registerItem("clockwork_core",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item GEM_HOLSTER = registerItem("gem_holster",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item ACID_MIXER = registerItem("acid_mixer",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item ELECTROLYZER = registerItem("electrolyzer",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     //iron set
     public static final Item STEEL_ACTUATOR = registerItem("steel_actuator",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item STEEL_CABLE = registerItem("steel_cable",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(4)));
     public static final Item STEEL_FRAME = registerItem("steel_frame",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(4)));
     public static final Item STEEL_DETOXIFIER = registerItem("steel_detoxifier",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item STEEL_DISTRIBUTOR = registerItem("steel_distributor",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item BOILER = registerItem("boiler",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().maxCount(1)));
     public static final Item NUTRIENT_MIXER = registerItem("nutrient_mixer",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     //crystal set
     public static final Item CRYSTAL_ROD = registerItem("crystal_rod",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item CRYSTAL_CORE = registerItem("crystal_core",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item SHARDED_RIB = registerItem("sharded_rib",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(4)));
 
     //computer parts
     public static final Item CIRCUITRY = registerItem("circuitry",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item SUPER_CAPACITOR = registerItem("super_capacitor",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     //rejuvenated parts
     public static final Item REJUVENATED_RIB = registerItem("rejuvenated_rib",
-            new GlintItem(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(4)));
+            new GlintItem(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(4)));
     public static final Item REJUVENATED_SPINE = registerItem("rejuvenated_spine",
-            new GlintItem(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new GlintItem(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item REANIMATED_HEART = registerItem("reanimated_heart",
-            new GlintItem(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new GlintItem(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item REANIMATED_KIDNEY = registerItem("reanimated_kidney",
-            new GlintItem(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new GlintItem(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     //utility
     public static final Item CAPTURED_SOUL = registerItem("captured_soul",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item CREATIVE_SOUL = registerItem("creative_soul",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item IRON_LUNG = registerItem("iron_lung",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item HEART_OF_GOLD = registerItem("heart_of_gold",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item HASTY_MUSCLE = registerItem("hasty_muscle",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(16)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(16)));
     public static final Item SWIM_BLADDER = registerItem("swim_bladder",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item RESONANT_CORE = registerItem("resonant_core",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item ADRENAL_CATALYST = registerItem("adrenal_catalyst",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item ADRENAL_SHRIEKER = registerItem("adrenal_shrieker",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item STEEL_STOMACH = registerItem("steel_stomach",
-            new DoniItem(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new DoniItem(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item STEEL_RUMEN = registerItem("steel_rumen",
-            new DoniItem(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new DoniItem(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     public static final Item BENIGN_TUMOR = registerItem("benign_tumor",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     public static final Item STEM_CELL_CANISTER = registerItem("stem_cell_canister",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     public static final Item CLUSTER_BOMB = registerItem("cluster_bomb",
-            new Item(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new Item(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
     //I have zero idea how to make tumor work, so I've removed its recipe and all child recipes
     public static final Item DRACONIS_FUNDAMENTUM = registerItem("draconis_fundamentum",
-            new GlintItem(new FabricItemSettings().group(ChestCavity.ORGAN_ITEM_GROUP).maxCount(1)));
+            new GlintItem(new FabricItemSettings().group(CCAstroAdds.ORGAN_ITEM_GROUP).maxCount(1)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(CCAstroAdds.MOD_ID, name), item);
